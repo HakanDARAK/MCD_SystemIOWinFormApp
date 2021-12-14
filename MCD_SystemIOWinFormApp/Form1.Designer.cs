a7c51bad27383d80085a3ac765c9cbfddf89185a
HakanDARAK <hakandarak1@gmail.com>
2021-12-14 12:11:15 +0300

    Proje dosyası ekle.

---
+++ MCD_SystemIOWinFormApp/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace MCD_SystemIOWinFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtUlke = new System.Windows.Forms.TextBox();
+            this.txtFirma = new System.Windows.Forms.TextBox();
+            this.txtEmailAdres = new System.Windows.Forms.TextBox();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPersonelKaydet = new System.Windows.Forms.Button();
+            this.btnPersonelGetir = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstPersonel = new System.Windows.Forms.ListBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtUlke);
+            this.groupBox2.Controls.Add(this.txtFirma);
+            this.groupBox2.Controls.Add(this.txtEmailAdres);
+            this.groupBox2.Controls.Add(this.txtSoyisim);
+            this.groupBox2.Controls.Add(this.txtIsim);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(218, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(297, 183);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Personel Detay";
+            // 
+            // txtUlke
+            // 
+            this.txtUlke.Location = new System.Drawing.Point(80, 125);
+            this.txtUlke.Name = "txtUlke";
+            this.txtUlke.Size = new System.Drawing.Size(196, 20);
+            this.txtUlke.TabIndex = 1;
+            // 
+            // txtFirma
+            // 
+            this.txtFirma.Location = new System.Drawing.Point(80, 99);
+            this.txtFirma.Name = "txtFirma";
+            this.txtFirma.Size = new System.Drawing.Size(196, 20);
+            this.txtFirma.TabIndex = 1;
+            // 
+            // txtEmailAdres
+            // 
+            this.txtEmailAdres.Location = new System.Drawing.Point(80, 76);
+            this.txtEmailAdres.Name = "txtEmailAdres";
+            this.txtEmailAdres.Size = new System.Drawing.Size(196, 20);
+            this.txtEmailAdres.TabIndex = 1;
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(80, 50);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(196, 20);
+            this.txtSoyisim.TabIndex = 1;
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(80, 24);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(196, 20);
+            this.txtIsim.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Ülke:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Firma:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email Adres:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyisim:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Isim:";
+            // 
+            // btnPersonelKaydet
+            // 
+            this.btnPersonelKaydet.Location = new System.Drawing.Point(18, 415);
+            this.btnPersonelKaydet.Name = "btnPersonelKaydet";
+            this.btnPersonelKaydet.Size = new System.Drawing.Size(188, 23);
+            this.btnPersonelKaydet.TabIndex = 6;
+            this.btnPersonelKaydet.Text = "Personel Kaydet";
+            this.btnPersonelKaydet.UseVisualStyleBackColor = true;
+            this.btnPersonelKaydet.Click += new System.EventHandler(this.btnPersonelKaydet_Click);
+            // 
+            // btnPersonelGetir
+            // 
+            this.btnPersonelGetir.Location = new System.Drawing.Point(18, 374);
+            this.btnPersonelGetir.Name = "btnPersonelGetir";
+            this.btnPersonelGetir.Size = new System.Drawing.Size(188, 23);
+            this.btnPersonelGetir.TabIndex = 5;
+            this.btnPersonelGetir.Text = "Personel Getir";
+            this.btnPersonelGetir.UseVisualStyleBackColor = true;
+            this.btnPersonelGetir.Click += new System.EventHandler(this.btnPersonelGetir_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstPersonel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 344);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personel Liste";
+            // 
+            // lstPersonel
+            // 
+            this.lstPersonel.FormattingEnabled = true;
+            this.lstPersonel.Location = new System.Drawing.Point(6, 20);
+            this.lstPersonel.Name = "lstPersonel";
+            this.lstPersonel.Size = new System.Drawing.Size(188, 316);
+            this.lstPersonel.TabIndex = 0;
+            this.lstPersonel.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstPersonel_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 453);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnPersonelKaydet);
+            this.Controls.Add(this.btnPersonelGetir);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtUlke;
+        private System.Windows.Forms.TextBox txtFirma;
+        private System.Windows.Forms.TextBox txtEmailAdres;
+        private System.Windows.Forms.TextBox txtSoyisim;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPersonelKaydet;
+        private System.Windows.Forms.Button btnPersonelGetir;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstPersonel;
+    }
+}
+
